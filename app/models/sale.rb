0bf1252235cf9bca95f3cb8f70dd10341d8a6f9f
christinach <actspatial@gmail.com>
2017-08-24 07:07:37 -0400

    commented out the CompanySerializer

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,5 @@
 class Sale < ApplicationRecord
-	include ActiveModel::Serializers::JSON
+	#include ActiveModel::Serializers::JSON
 	belongs_to :company
 	validates_presence_of :day1, :day2, :day3, :day4, :day5, :day6, :day7, :company_id
 end
