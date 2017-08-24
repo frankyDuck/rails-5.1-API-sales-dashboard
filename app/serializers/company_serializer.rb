e3d769bc852d2a822694d47e92e68a41fc14d4e9
christinach <actspatial@gmail.com>
2017-08-24 07:11:28 -0400

    silly circular call

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,3 +1,3 @@
-class CompanySerializer < ActiveModel::CompanySerializer
+class CompanySerializer < ActiveModel::Serializer
   attributes :name, :start_week, :end_week, :city, :state, :country_code, :time_zone 
 end
